7db5667c4b1a6e15860eb212c7731ca586e86175
Ahmad Azzam <ahmadwaleedazzam123@gmail.com>
2025-07-26 15:52:28 +0400

    add EditNoteView

--- lib/widgets/custom_note_item.dart
+++ lib/widgets/custom_note_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:note_app/views/edit_note_view.dart';
 
 class CustomNoteItem extends StatelessWidget {
   const CustomNoteItem({super.key});
@@ -7,45 +8,57 @@ class CustomNoteItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.only(bottom: 12),
-      child: Container(
-        padding: EdgeInsets.only(top: 24, bottom: 24, left: 12),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(16),
-          color: const Color(0xffFFCC80),
-        ),
+      child: GestureDetector(
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) {
+                return EditNoteView();
+              },
+            ),
+          );
+        },
+        child: Container(
+          padding: EdgeInsets.only(top: 24, bottom: 24, left: 12),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(16),
+            color: const Color(0xffFFCC80),
+          ),
 
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: [
-            ListTile(
-              title: Text(
-                'Flutter tips',
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 22,
-                  fontWeight: FontWeight.bold,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              ListTile(
+                title: Text(
+                  'Flutter tips',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
+
+                subtitle: Padding(
+                  padding: const EdgeInsets.only(top: 15),
+                  child: Text(
+                    'Build your career with ahmad azzam',
+                    style: TextStyle(color: Colors.black.withAlpha(100)),
+                  ),
+                ),
+
+                trailing: Icon(Icons.delete, size: 25, color: Colors.black),
               ),
 
-              subtitle: Padding(
-                padding: const EdgeInsets.only(top: 15),
+              Padding(
+                padding: const EdgeInsets.only(right: 24),
                 child: Text(
-                  'Build your career with ahmad azzam',
+                  'Jul 22,2025',
                   style: TextStyle(color: Colors.black.withAlpha(100)),
                 ),
               ),
-
-              trailing: Icon(Icons.delete, size: 25, color: Colors.black),
-            ),
-
-            Padding(
-              padding: const EdgeInsets.only(right: 24),
-              child: Text(
-                'Jul 22,2025',
-                style: TextStyle(color: Colors.black.withAlpha(100)),
-              ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
